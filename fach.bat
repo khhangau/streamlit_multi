2fdb7c59f96463a41a2ef4b453a316b5d1a734d0
Kim Hang <Kim.Hang@moretonbay.qld.gov.au>
2021-07-16 11:18:45 +1000

    streamlit_multi

--- fach.bat
+++ fach.bat
@@ -1,9 +1,4 @@
-rem git fetch
-rem git checkout -m master wbnm.py
+git fetch
+git checkout -m master wbnm.py
 rem git add wbnm.py
 rem git commit
-
-git add .
-git commit -m "streamlit_multi"
-git pull https://github.com/khhangau/streamlit_multi.git master
-git push --set-upstream origin master
